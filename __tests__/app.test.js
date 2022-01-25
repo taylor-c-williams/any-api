0a23b2417b062e877a7ae9de251322074f66a544
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-25 10:51:03 -0800

    5 tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,4 +24,40 @@ describe('any-api routes', () => {
     });
   });
 
+  it('fetches a cat by her ID', async () => {
+    const cat = await Cat.insert({ breed: 'Maine Coon', origin: 'Maine' });
+
+    const res = await request(app).get(`/api/v1/cats/${cat.id}`);
+
+    expect(res.body).toEqual(cat);
+  });
+
+  it('fetches all cats', async () => {
+    const cat1 = await Cat.insert({ breed: 'Maine Coon', origin: 'Maine' });
+
+    const cat2 = await Cat.insert({ breed: 'Norwegian Forest Cat', origin: 'Norway' });
+
+    const res = await request(app).get('/api/v1/cats');
+
+    expect(res.body).toEqual([cat1, cat2]);
+  });
+
+  it('updates a single cat by her ID', async () => {
+    const cat1 = await Cat.insert({ breed: 'Maine Coon', origin: 'Maine' });
+
+    const res = await request(app)
+      .patch(`/api/v1/cats/${cat1.id}`)
+      .send({ origin: 'United States' });
+
+    expect(res.body).toEqual({ ...cat1, origin: 'United States' });
+  });
+
+  it('deletes a cat by ID', async () => {
+    const cat = await Cat.insert({ breed: 'Maine Coon', origin: 'Maine' });
+    const res = await request(app).delete(`/api/v1/cats/${cat.id}`);
+
+    expect(res.body).toEqual(cat);
+    expect(await Cat.getById(cat.id)).toBeNull();
+  });
+
 });
