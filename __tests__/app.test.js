1d850717f41702adc2e85b3f0afd99efc647f028
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-24 17:57:15 -0800

    controller, model and first test initalized

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Cat = require('../lib/models/Cat');
 
 describe('any-api routes', () => {
   beforeEach(() => {
@@ -11,4 +12,16 @@ describe('any-api routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+
+  it('creates a cat entry', async () => {
+    const res = await Cat.insert({ breed:'Norwegian Forest Cat', origin: 'Norway' });
+
+    expect(res).toEqual({
+      id: expect.any(String),
+      breed:'Norwegian Forest Cat',
+      origin: 'Norway'      
+    });
+  });
+
 });
